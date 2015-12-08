5ae423ab342965e3f543e8eb160a9a29362a0eae
JiangInk <JiangInk@foxmail.com>
2015-12-08 16:29:53 +0800

    kindEditor编辑器添加

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -54,5 +54,79 @@ $(document).ready(function () {
     $("button#cancel").click(function () {
         history.back(-1);
     });
+
+    //KindEditor
+    KindEditor.ready(function(K) {
+        K.each({
+            'plug-align' : {
+                name : '对齐方式',
+                method : {
+                    'justifyleft' : '左对齐',
+                    'justifycenter' : '居中对齐',
+                    'justifyright' : '右对齐'
+                }
+            },
+            'plug-order' : {
+                name : '编号',
+                method : {
+                    'insertorderedlist' : '数字编号',
+                    'insertunorderedlist' : '项目编号'
+                }
+            },
+            'plug-indent' : {
+                name : '缩进',
+                method : {
+                    'indent' : '向右缩进',
+                    'outdent' : '向左缩进'
+                }
+            }
+        },function( pluginName, pluginData ){
+            var lang = {};
+            lang[pluginName] = pluginData.name;
+            KindEditor.lang( lang );
+            KindEditor.plugin( pluginName, function(K) {
+                var self = this;
+                self.clickToolbar( pluginName, function() {
+                    var menu = self.createMenu({
+                        name : pluginName,
+                        width : pluginData.width || 100
+                    });
+                    K.each( pluginData.method, function( i, v ){
+                        menu.addItem({
+                            title : v,
+                            checked : false,
+                            iconClass : pluginName+'-'+i,
+                            click : function() {
+                                self.exec(i).hideMenu();
+                            }
+                        });
+                    })
+                });
+            });
+        });
+        K.create('textarea', {
+            themeType : 'qq',
+            items : [
+                'bold','italic','underline','fontname','fontsize','forecolor','hilitecolor','plug-align','plug-order','plug-indent','link'
+            ]
+        });
+    });
+
+    //text-overflow hidden
+    var $blogContent = $("div.blog-content");
+    $blogContent.each(function(i) {
+        var sContent = $(this).html(),
+            $span = $("<span></span>"),
+            $a = $("<a></a>");
+
+        this.index = i;
+        $span.html(sContent.substring(0, 100));
+        $a.html(sContent.length > 150 ? '...' : '');
+        $a.css({"text-decoration": "none"});
+        $(this).html('');
+        $(this).append($span);
+        $(this).append($a);
+    });
+
 });
 
